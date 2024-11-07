0c1595dce5bdc4014bf74aa61599eff22f60ce49
Lucas Dauto <lucasdauto@gmail.com>
2024-11-07 01:42:34 -0300

    terminal

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-require("dotenv").config();
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { fazerPergunta } from './pergunta.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 // Set up your API key as an environment variable
-geminiKey = process.env.GEMINI_API_KEY;
+const geminiKey = process.env.GEMINI_API_KEY;
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(geminiKey);
@@ -11,7 +15,10 @@ async function run() {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-  const prompt = "Me fale sobre o salvador"
+  let prompt = "Você é um site de viagens e deve responder somente sobre esse assunto."+
+  "Caso o usuario pergunte sobre algo diferente. Diga que não pode reponder sobre esse assunto."+
+  "O usuario respondeu: ";
+  prompt += await fazerPergunta("Me falo sobre o destino que deseja conhecer: ");
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
